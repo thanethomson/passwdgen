862e3bbc38a9fb2ecb7e6fc9d8e5f909370e1cc9
Thane Thomson <connect@thanethomson.com>
2019-11-04 15:35:03 -0500

    Fix PyPI README and manifest

--- setup.py
+++ setup.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python
 
+from io import open
+import os.path
 from setuptools import setup
 
 INSTALL_REQUIREMENTS = [
     "pyperclip==1.7.0"
 ]
 
+
+def read_file(filename):
+    full_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), filename)
+    with open(full_path, "rt", encoding="utf-8") as f:
+        lines = f.readlines()
+    return lines
+
+
 setup(
     name="passwdgen",
-    version="0.3.0",
+    version="0.3.1",
     description="A password generation utility",
+    long_description="".join(read_file("README.md")),
+    long_description_content_type="text/markdown",
     author="Thane Thomson",
     author_email="connect@thanethomson.com",
     url="https://github.com/thanethomson/passwdgen",
